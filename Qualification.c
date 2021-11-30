f3e5611dac238498ac0c28196c040a452b77a727
Deadpool27 <44410726+Deadpool2701@users.noreply.github.com>
2021-11-30 18:41:31 +0530

    Create Qualification.c
    Snackdown 2019 is coming! There are two rounds (round A and round B) after the qualification round. From both of them, teams can qualify to the pre-elimination round. According to the rules, in each of these two rounds, teams are sorted in descending order by their score and each team with a score greater or equal to the score of the team at the K=1500-th place advances to the pre-elimination round (this means it is possible to have more than K qualified teams from each round in the case of one or more ties after the K-th place).
    
    Today, the organizers ask you to count the number of teams which would qualify for the pre-elimination round from round A for a given value of K (possibly different from 1500). They provided the scores of all teams to you; you should ensure that all teams scoring at least as many points as the K-th team qualify.

---
+++ Qualification.c
@@ -0,0 +1,30 @@
+//
+#include <bits/stdc++.h>
+#define ll long long 
+using namespace std;
+
+int main() {
+	
+	ll t;
+	cin>>t;
+	while(t--){
+	    ll n,k,c=0;
+	    cin>>n>>k;
+	    ll a[n];
+	    for(ll i=0;i<n;i++)
+	    cin>>a[i];
+	   sort(a, a + n, greater<>());
+	    ll score=a[k-1];
+	    for(ll i=0;i<n;i++){
+	        if(a[i]>=score){
+	            c++;
+	        }
+	        else
+	        {
+	            break;
+	        }
+	    }
+	    cout<<c<<endl;
+	}
+	return 0;
+}
